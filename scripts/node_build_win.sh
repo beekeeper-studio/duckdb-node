71985f49731f18cac6e6856f38e661ee42a2ad5c
Mohammad Azmi <mazmiagus2003@gmail.com>
2024-07-23 11:35:17 +0700

    Update node_build_win.sh

--- scripts/node_build_win.sh
+++ scripts/node_build_win.sh
@@ -31,16 +31,17 @@ npx node-pre-gyp package testpackage testbinary
 
 LOCAL_BINARY=$(./node_modules/.bin/node-pre-gyp reveal staged_tarball --silent)
 REMOTE_BINARY=$(./node_modules/.bin/node-pre-gyp reveal hosted_tarball --silent)
-S3_ENDPOINT_BINARY="s3://duckdb-npm/"${REMOTE_BINARY:23}
+# S3_ENDPOINT_BINARY="s3://duckdb-npm/"${REMOTE_BINARY:23}
 
-pip install awscli
+# pip install awscli
 
 echo "local binary at  $LOCAL_BINARY"
 echo "remote binary at $REMOTE_BINARY"
-echo "served from      $S3_ENDPOINT_BINARY"
+# echo "served from      $S3_ENDPOINT_BINARY"
 
-if [[ "$GITHUB_REF" =~ ^(refs/heads/main|refs/tags/v.+)$ ]] ; then
+' if [[ "$GITHUB_REF" =~ ^(refs/heads/main|refs/tags/v.+)$ ]] ; then
   aws s3 cp $LOCAL_BINARY $S3_ENDPOINT_BINARY --acl public-read
 else
   aws s3 cp $LOCAL_BINARY $S3_ENDPOINT_BINARY --acl public-read --dryrun
 fi
+'
